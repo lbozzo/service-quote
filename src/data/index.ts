f4e22653e05983343191058b735b1b6e1b2d380d
Lucas Bozzo <lucas.bozzo@Lucass-Mac-mini.local>
2024-04-28 02:53:01 +0100

    Fix

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,8 @@
+import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client/web';
+
+config({ path: '.dev.vars' });
 
 const turso = createClient({
   url: process.env.TURSO_DATABASE_URL!,
--- src/data/migrate.ts
+++ src/data/migrate.ts
@@ -1,10 +1,5 @@
-// migrate.ts
-
-import { config } from 'dotenv';
 import { migrate } from 'drizzle-orm/libsql/migrator';
 import { data } from './index';
-
-config({ path: '.dev.vars' });
 
 const main = async () => {
   try {
@@ -14,7 +9,6 @@ const main = async () => {
   } catch (error) {
     console.error('Migration failed');
     console.error(error);
-    process.exit(1);
   }
   process.exit(0);
 };
