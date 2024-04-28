f6bd8d6ec5b75442d462d5ed2828430e7791b3bd
Lucas Bozzo <lucas.bozzo@Lucass-Mac-mini.local>
2024-04-28 02:44:01 +0100

    Include migrations

---
+++ migrations/0000_wild_revanche.sql
@@ -0,0 +1,14 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+CREATE TABLE `foo` (
+	`bar` text DEFAULT 'Hey!' NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `__drizzle_migrations` (
+	`id` numeric PRIMARY KEY,
+	`hash` text NOT NULL,
+	`created_at` numeric
+);
+
+*/
